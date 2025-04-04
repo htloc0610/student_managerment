918ef9ad1084c2cd7492a3b755c2e722127f40d8
locht1 <locht1@runsystem.net>
2025-04-04 13:20:43 +0700

    first commit

---
+++ src/main/java/vn/member_managerment/ManagerMemberApplication.java
@@ -0,0 +1,13 @@
+package vn.member_managerment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerMemberApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/member_managerment/ManagerMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.member_managerment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
